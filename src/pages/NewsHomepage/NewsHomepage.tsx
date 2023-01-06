b487c537e89e42853502e9d687c26057ca6e0e57
j-novaes <joaovictor.nb21@hotmail.com>
2023-01-06 00:43:02 +0000

    Added trending section

--- src/pages/NewsHomepage/NewsHomepage.tsx
+++ src/pages/NewsHomepage/NewsHomepage.tsx
@@ -1,14 +1,58 @@
 // Style
-import StyledNewsHomePage from "./StyledNewsHomepage";
+import StyledNewsHomepage from "./StyledNewsHomepage";
+
+// Components
+import NewsItem from "./components/NewsItem/NewsItem";
+
 
 // Images
 import introDesktop from './imgs/intro-desktop.jpg';
+import introMobile from './imgs/intro-mobile.jpg';
+import pc from './imgs/pc.jpg';
+import keyboard from './imgs/keyboard.jpg';
+import gamer from './imgs/gamer.jpg';
+import TrendingItem from "./components/TrendingItem/TrendingItem";
+import {Logo} from './imgs/Icons'
 
 const NewsHomepage = () => {
+
+    const news = [
+        {
+            title: 'Hydrogen VS Electric Cars',
+            text: 'Will hydrogen-fueled cars ever catch up to EVs?',
+        },
+        {
+            title: 'The Downsides of AI Artistry',
+            text: 'What are the possible adverse effects of on-demand AI image generation?',
+        },
+        {
+            title: 'Is VC Funding Drying Up?',
+            text: 'Private funding by VC firms is down 50% YOY. We take a look at what that means.',
+        }
+    ]
+
+    const trending = [
+        {
+            image: pc,
+            title: 'Reviving Retro PCs',
+            text: 'What happens when old PCs are given modern upgrades?',
+        },
+        {
+            image: keyboard,
+            title: 'Top 10 Laptops of 2022',
+            text: 'Our best picks for various needs and budgets.',
+        },
+        {
+            image: gamer,
+            title: 'The Growth of Gaming',
+            text: 'How the pandemic has sparked fresh opportunities.',
+        }
+    ]
+
     return (
-        <StyledNewsHomePage>
+        <StyledNewsHomepage introDesktopImage={introDesktop} introMobileImage={introMobile}>
             <section className='navbar'>
-                <img className='logo' src='' />
+                <Logo />
                 <nav className='links'>
                     <a className='nav-link' href='#'>Home</a>
                     <a className='nav-link' href='#'>News</a>
@@ -20,9 +64,9 @@ const NewsHomepage = () => {
 
             <section className='hero'>
                 <section className='intro'>
-                    <img className='intro-image' src={introDesktop} />
+                    <section className='intro-image'></section>
                     <h1 className='intro-title'>The Bright Future of Web 3.0?</h1>
-                    <div className=''>
+                    <div className='intro-invite'>
                         <p className='intro-about'>We dive into the next evolution of the web that claims to put the power of the platforms back into the hands of the people.
                             But is it really fulfilling its promise?</p>
                         <button className='intro-btn'>READ MORE</button>
@@ -31,12 +75,18 @@ const NewsHomepage = () => {
 
                 <section className='news'>
                     <h2 className='news-title'>New</h2>
-                    <article className='news-item'>
-                        <h3 className='news-item-title'>Hydrogen VS Electric Cars</h3>
-                    </article>
+                    {news.map(n => <NewsItem title={n.title} text={n.text} />)}
                 </section>
             </section>
-        </StyledNewsHomePage>
+
+            <section className='trending'>
+                {
+                    trending.slice(0, 3).map((t, index) => {
+                        return <TrendingItem image={t.image} rank={index + 1} title={t.title} text={t.text} />
+                    })
+                }
+            </section>
+        </StyledNewsHomepage>
     )
 }
 
